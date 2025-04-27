67011ba14c84c2f25016397dc313b0c18c969236
arremadhav <madhavnimbola123@gmail.com>
2025-04-27 16:18:01 +0530

    Initial commit for backend dev/ v2 branch

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import connectDB from './config/connectDB.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+connectDB();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ hello: "world" });
+});
+
+// Routes
+import authRoutes from './routes/authRoutes.js';
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
